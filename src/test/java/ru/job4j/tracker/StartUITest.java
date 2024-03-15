e67788cbc01b117e9032f205bd940cb9014764f1
vladoccc <vladrma777@gmail.com>
2024-03-15 16:39:33 +0300

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.core.IsNull.nullValue;
 
 class StartUITest {
 
