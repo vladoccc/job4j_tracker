3ae195cf51b64c34a9591e644dde5cf8232592b8
vladoccc <vladrma777@gmail.com>
2024-04-19 15:26:40 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                result = i;
+                break;
+            }
+            if (result == -1) {
+                throw new ElementNotFoundException("Элемента в массиве нет.");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] array = {"cat", "dog", "monkey"};
+        try {
+            indexOf(array, "snake");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
