a53003b17ef348e90cb1b4c5235f4cc1f53729a5
vladoccc <vladrma777@gmail.com>
2024-04-26 09:34:16 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #492905]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -4,7 +4,6 @@ public class PasswordValidator {
 
     private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
 
-    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
