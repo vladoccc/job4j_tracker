cb76e3c5fca9af34dcafe90b644bb7caaf31ee00
Влад Радченко <vladrma777@gmail.com>
2024-07-03 13:06:04 +0300

    5. Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.SplittableRandom;
+
+public class Task {
+
+    private String description;
+    private int priority;
+
+    public Task(String description, int priority) {
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+}
