9fedfaa28352af1987eeba8d2d23fc8303071ef2
Влад Радченко <vladrma777@gmail.com>
2024-07-04 09:41:22 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #503538]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -9,7 +9,9 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
+            if (element.getPriority() > task.getPriority()) {
+                break;
+            } else {
                 index += 1;
             }
         }
