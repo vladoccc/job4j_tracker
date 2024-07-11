a6dfaa90d6bfe1aa104cbfe393b31faf290f2397
Влад Радченко <vladrma777@gmail.com>
2024-07-11 09:09:02 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String line : origin) {
+            check.add(line);
+        }
+        for (String text1 : text) {
+            result = check.contains(text1);
+            break;
+        }
+        return result;
+    }
+}
