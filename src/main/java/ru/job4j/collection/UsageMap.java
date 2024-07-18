2af7687e0c0b041e725c4992881c44ba69771f34
Влад Радченко <vladrma777@gmail.com>
2024-07-18 13:12:53 +0300

    0. Map, HashMap [#212652 #505428]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> name = new HashMap<>();
         name.put("vladrma@gmail.com", "Vladislav Olegovich");
+        name.put("igors@gmail.com", "Igor Sergeevich");
+        name.put("sobaka@gmail.com", "Ivan Ivanovich");
+        name.put("sobaka@gmail.com", "Ivan Petrovich");
+        name.put("sergey@gmail.com", "Sergey Petrovich");
         for (String key : name.keySet()) {
             String value = name.get(key);
             System.out.println(key + ": " + value);
