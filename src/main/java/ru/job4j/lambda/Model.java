490f9ba0f459f53812130ebb1c039a72a0f1c3a5
Влад Радченко <vladrma777@gmail.com>
2024-08-09 16:33:29 +0300

    4. Ссылки на конструкторы [#504922]

---
+++ src/main/java/ru/job4j/lambda/Model.java
@@ -0,0 +1,17 @@
+package ru.job4j.lambda;
+
+public class Model {
+
+    private String name;
+
+    public Model() {
+    }
+
+    public Model(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
